1af74a9fa6ecf1da5e01063010f8f0e13ec14593
Shawn Lee <changoloco.evg@gmail.com>
2022-05-10 10:02:22 -0500

    :fire: [refactor]: delete unused assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,3 @@
 export { default as avatar } from './avatar-michelle.jpg';
 export { default as drawers } from './drawers.jpg';
-export { default as iFacebook } from './icon-facebook.svg';
-export { default as iPinterest } from './icon-pinterest.svg';
 export { default as iShare } from './icon-share.svg';
-export { default as iTwitter } from './icon-twitter.svg';
